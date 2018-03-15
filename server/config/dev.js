b672de85e456e4b5ae4faf01f0ce08f47c6dd3ea
prateek211205361719 <prateek@ceptes.com>
2018-03-15 15:13:29 +0530

    Delete dev.js

--- server/config/dev.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    clientID: "228619871922-akgmuon5l9n35pb5cdbb5hlgt50ab84o.apps.googleusercontent.com",
-    clientSecret: "1mVLJVxizUJFuoXTofc2UylF",
-    callbackURL: "/auth/google/callback",
-    mongoURI: "mongodb://prateek211205:sanu211205@ds163053.mlab.com:63053/feedback_dev"
-};
